fe40691ddac331e3f1ba6568762725152b94e557
Xiaoqiu HE <hexiaoqiu@gmail.com>
2025-01-25 21:32:07 +0100

    12/asmSelectRoot2: add the option MyPC for working on my dell laptop

--- 12_getCaseDirList/asmSelectRoot2.m
+++ 12_getCaseDirList/asmSelectRoot2.m
@@ -96,17 +96,36 @@ function [configList] = asmSelectRoot2(tag)
             path = fullfile(root,folderName);
             [configList{7}] = getCaseDirListRc09(path);
 
-        case 'NAS'
-            path = '/Volumes/00_BubbleDNSData/00_Raw/03_Bubble+/GoodCaseCollection/00_A_0.02_Rc_0.01';
-            [configList{1}] = getCaseDirListRc001(path);
-            path = '/Volumes/00_BubbleDNSData/00_Raw/03_Bubble+/GoodCaseCollection/01_A_0.2_Rc_0.1';
-            [configList{2}] = getCaseDirListRc01(path);
-            path = '/Volumes/00_BubbleDNSData/00_Raw/03_Bubble+/GoodCaseCollection/02_A_0.55_Rc_0.3';
-            [configList{3}] = getCaseDirListRc03(path);
-            path = '/Volumes/00_BubbleDNSData/00_Raw/03_Bubble+/GoodCaseCollection/03_A_0.8_Rc_0.5';
-            [configList{4}] = getCaseDirListRc05(path);
-            path = '/Volumes/00_BubbleDNSData/00_Raw/03_Bubble+/GoodCaseCollection/04_A_0.99_Rc_0.9';
-            [configList{5}] = getCaseDirListRc09(path);
+        case 'MyPC'
+            root = 'X:\00_Raw\03_Bubble+\GoodCaseCollection';
+            
+            folderName = '01_A_50_Rc_0.01';
+            path = fullfile(root,folderName);
+            [configList{1}] = getCaseDirListRc001_2(path);
+            
+            folderName = '02_A_5_Rc_0.1';
+            path = fullfile(root,folderName);
+            [configList{2}] = getCaseDirListRc01_2(path);
+            
+            folderName = '03_A_4';
+            path = fullfile(root,folderName);
+            [configList{3}] = getCaseDirListA4(path);
+
+            folderName = '04_A_3';
+            path = fullfile(root,folderName);
+            [configList{4}] = getCaseDirListA3(path);
+            
+            folderName = '05_A_1.82_Rc_0.3';
+            path = fullfile(root,folderName);
+            [configList{5}] = getCaseDirListRc03(path);
+
+            folderName = '06_A_1.25_Rc_0.5';
+            path = fullfile(root,folderName);
+            [configList{6}] = getCaseDirListRc05(path);
+
+            folderName = '07_A_1_Rc_0.9';
+            path = fullfile(root,folderName);
+            [configList{7}] = getCaseDirListRc09(path);
         case 'HDD'
             path = '/Volumes/Silver16TB/03_Bubble+/GoodCaseCollection/00_A_0.02_Rc_0.01';
             [configList{1}] = getCaseDirListRc001(path);
