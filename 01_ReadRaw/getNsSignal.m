947428819bb4edc24019eabd9756e5dffbbbf5e3
Xiaoqiu HE <xiaoqiuhe@hust.edu.cn>
2025-07-15 07:58:04 +0200

    Improve the runtime check code: getNsSignal.m

--- 01_ReadRaw/getNsSignal.m
+++ 01_ReadRaw/getNsSignal.m
@@ -7,46 +7,48 @@ function [signal] = getNsSignal(caseRawDir)
     fid = fopen(fileLocation);
     % Compute Line number
     row=0;
-    while ~feof(fid) % ÊòØÂê¶ËØªÂèñÂà∞Êñá‰ª∂ÁªìÂ∞?
-        [~]=fgets(fid); % ÊàñË?Öfgetl
-        row=row+1; % Ë°åÊï∞Á¥ØÂä†
+    while ~feof(fid) % òØÂê¶ËØªñà∞Êñá‰ª∂ÁìÂ∞?
+        [~]=fgets(fid); % àñ?Öfgetl
+        row=row+1; % Ë°åï∞Á¥Øä
     end
-    fclose(fid); % ÂèäÊó∂ÂÖ≥Èó≠Êñá‰ª∂ÊòØ‰∏™Â•Ω‰π†ÊÉ?
+    fclose(fid); % äó∂ÂÖ≥Èó≠Êñá‰ª∂ÊòØ‰∏™Â•Ω‰°°É?
 
     capacity = (row-4)*31;
     fid = fopen(fileLocation);
     data = textscan(fid, '%f', capacity, 'HeaderLines', 4);
     fclose(fid);
-    signal.time = data{1}(1:31:end-31+1);
-    signal.P1 = data{1}(2:31:end-31+2);
-    signal.P2 = data{1}(3:31:end-31+3);
-    signal.P3 = data{1}(4:31:end-31+4);
-    signal.P4 = data{1}(5:31:end-31+5);
-    signal.P5 = data{1}(6:31:end-31+6);
-    signal.P6 = data{1}(7:31:end-31+7);
-    signal.T1 = data{1}(8:31:end-31+8);
-    signal.T2 = data{1}(9:31:end-31+9);
-    signal.T3 = data{1}(10:31:end-31+10);
-    signal.T4 = data{1}(11:31:end-31+11);
-    signal.T5 = data{1}(12:31:end-31+12);
-    signal.T6 = data{1}(13:31:end-31+13);
-    signal.U1 = data{1}(14:31:end-31+14);
-    signal.U2 = data{1}(15:31:end-31+15);
-    signal.U3 = data{1}(16:31:end-31+16);
-    signal.U4 = data{1}(17:31:end-31+17);
-    signal.U5 = data{1}(18:31:end-31+18);
-    signal.U6 = data{1}(19:31:end-31+19);
-    signal.V1 = data{1}(20:31:end-31+20);
-    signal.V2 = data{1}(21:31:end-31+21);
-    signal.V3 = data{1}(22:31:end-31+22);
-    signal.V4 = data{1}(23:31:end-31+23);
-    signal.V5 = data{1}(24:31:end-31+24);
-    signal.V6 = data{1}(25:31:end-31+25);
-    signal.W1 = data{1}(26:31:end-31+26);
-    signal.W2 = data{1}(27:31:end-31+27);
-    signal.W3 = data{1}(28:31:end-31+28);
-    signal.W4 = data{1}(29:31:end-31+29);
-    signal.W5 = data{1}(30:31:end-31+30);
-    signal.W6 = data{1}(31:31:end-31+31);
+    collectInterval = 32;
+
+    signal.time = data{1}(1:collectInterval:end-collectInterval+1);
+    signal.P1 = data{1}(2:collectInterval:end-collectInterval+2);
+    signal.P2 = data{1}(3:collectInterval:end-collectInterval+3);
+    signal.P3 = data{1}(4:collectInterval:end-collectInterval+4);
+    signal.P4 = data{1}(5:collectInterval:end-collectInterval+5);
+    signal.P5 = data{1}(6:collectInterval:end-collectInterval+6);
+    signal.P6 = data{1}(7:collectInterval:end-collectInterval+7);
+    signal.T1 = data{1}(8:collectInterval:end-collectInterval+8);
+    signal.T2 = data{1}(9:collectInterval:end-collectInterval+9);
+    signal.T3 = data{1}(10:collectInterval:end-collectInterval+10);
+    signal.T4 = data{1}(11:collectInterval:end-collectInterval+11);
+    signal.T5 = data{1}(12:collectInterval:end-collectInterval+12);
+    signal.T6 = data{1}(13:collectInterval:end-collectInterval+13);
+    signal.U1 = data{1}(14:collectInterval:end-collectInterval+14);
+    signal.U2 = data{1}(15:collectInterval:end-collectInterval+15);
+    signal.U3 = data{1}(16:collectInterval:end-collectInterval+16);
+    signal.U4 = data{1}(17:collectInterval:end-collectInterval+17);
+    signal.U5 = data{1}(18:collectInterval:end-collectInterval+18);
+    signal.U6 = data{1}(19:collectInterval:end-collectInterval+19);
+    signal.V1 = data{1}(20:collectInterval:end-collectInterval+20);
+    signal.V2 = data{1}(21:collectInterval:end-collectInterval+21);
+    signal.V3 = data{1}(22:collectInterval:end-collectInterval+22);
+    signal.V4 = data{1}(23:collectInterval:end-collectInterval+23);
+    signal.V5 = data{1}(24:collectInterval:end-collectInterval+24);
+    signal.V6 = data{1}(25:collectInterval:end-collectInterval+25);
+    signal.W1 = data{1}(26:collectInterval:end-collectInterval+26);
+    signal.W2 = data{1}(27:collectInterval:end-collectInterval+27);
+    signal.W3 = data{1}(28:collectInterval:end-collectInterval+28);
+    signal.W4 = data{1}(29:collectInterval:end-collectInterval+29);
+    signal.W5 = data{1}(30:collectInterval:end-collectInterval+30);
+    signal.W6 = data{1}(31:collectInterval:end);
 
 end
